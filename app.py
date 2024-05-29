d66d3e84e43c98e3122b7b037667b41e0d457df4
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2024-05-30 02:14:53 +0300

    socketio for iframe

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import asyncio
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+# NEW
+from flask_socketio import SocketIO, emit
 from QuickAgent import ConversationManager, TextToSpeech, TranscriptCollector
 
 app = Flask(__name__)
+socketio = SocketIO(app, async_mode='eventlet') # Use eventlet for async support
 
 manager = ConversationManager()
 transcript_collector = TranscriptCollector()
@@ -27,10 +30,36 @@ async def process_transcription():
 def handle_transcribed_text(text):
     # Update the transcript collector with the transcribed text
     transcript_collector.add_part(text)
+    # New: Emit the transcribed text to the client via WebSocket
+    socketio.emit('transcription_update', {'text': text})
+    
 
 # Register the callback function with the ConversationManager
 manager.transcription_callback = handle_transcribed_text
 
+# Route to get the full transcript
+@app.route('/full_transcript', methods=['GET'])
+def get_full_transcript():
+    full_sentence = transcript_collector.get_full_transcript()
+    return jsonify({'full_sentence': full_sentence})
+    # return render_template('full_transcript.html', items=full_sentence)
+
+# WebSocket event handlers
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+    
+# @socketio.on('my event')
+# def handle_custom_event(json):
+#     print('Received event:' + str(json))
+#     emit('my response', {'data': 'response from server' })
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(app.run(debug=True))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(app.run(debug=True))
+    socketio.run(app, debug=True)
+    
