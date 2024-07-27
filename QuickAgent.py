4f656640191e689d6ef63923a1ee959a5498dc1c
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2024-07-27 18:45:25 +0300

    new dynamic background changer

--- QuickAgent.py
+++ QuickAgent.py
@@ -94,7 +94,6 @@ class LanguageModelProcessor:
 class TextToSpeech:
     
     DG_API_KEY = os.getenv("DEEPGRAM_API_KEY")
-    # MODEL_NAME = "aura-orion-en"  # Example model name, change as needed
     MODEL_NAME = "aura-orion-en"
 
     @staticmethod
